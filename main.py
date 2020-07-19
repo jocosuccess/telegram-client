db7829770eea679be003e9713459ba323a86a4fa
heroclass728 <highfamous@outlook.com>
2020-07-19 09:34:31 +0300

    add login ui

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ except ImportError:
     import Image
 import pytesseract as tess
 from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
+from telethon.errors import SessionPasswordNeededError, PhoneNumberUnoccupiedError
 import asyncio
 from datetime import date, datetime
 
-from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTableWidget, QDateTimeEdit, QTableWidgetItem, QVBoxLayout, QMessageBox, QWidget, QStackedWidget, QLineEdit, QInputDialog
+from PyQt5 import uic
 import sys
 from utils.constants import *
 
-from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
+from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import (GetHistoryRequest)
 from telethon.tl.functions.messages import (GetDialogsRequest)
 from telethon.tl.types import (PeerChannel, PeerChat, PeerUser)
@@ -32,60 +32,111 @@ config.read(CRED_DIR + '/' + credential_file)
 # Tessearct Environment
 tess.pytesseract.tesseract_cmd = config['Telegram']['tesseract_env']
 # Setting configuration values
-api_id = config['Telegram']['api_id']
-api_hash = config['Telegram']['api_hash']
+api_id = 695236
+api_hash = '358d0a4e0cfff381f9cec02e438ef7f0'
 
 api_hash = str(api_hash)
 
-phone = config['Telegram']['phone']
-username = config['Telegram']['username']
 
 # Create the client and connect
-client = TelegramClient(username, api_id, api_hash)
+client = TelegramClient("telegram", api_id, api_hash)
 
 categories = ['user', 'channel', 'group']
+window = None
 
 
-class Ui(QtWidgets.QMainWindow):
+class MainUI(QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi(GUI_DIR + '/' + gui_file, self)
-        # parameter
+        super(MainUI, self).__init__()
+        uic.loadUi(GUI_DIR + '/' + main_ui_file, self)
+        self.stacked_widget = QStackedWidget()
+        self.setCentralWidget(self.stacked_widget)
+        # self.check_sign()
+
+    # async def check_sign(self):
+    #     client.connect()
+    #     if client.is_user_authorized():
+    #     telegram_widget = TelegramWidget(self)
+    #     self.stacked_widget.addWidget(telegram_widget)
+        # else:
+        login_widget = LoginWidget(self)
+        self.stacked_widget.addWidget(login_widget)
+        # self.stacked_widget.setCurrentWidget(login_widget)
+        # self.show()
+
+    def show_telegram(self):
+        telegram_widget = TelegramWidget(self)
+        self.stacked_widget.addWidget(telegram_widget)
+        self.stacked_widget.setCurrentWidget(telegram_widget)
+
+
+class LoginWidget(QWidget):
+    def __init__(self, parent=None):
+        super(LoginWidget, self).__init__(parent)
+        uic.loadUi(GUI_DIR + '/' + login_ui_file, self)
+        self.phone_txt = self.findChild(QLineEdit, 'phone_txt')
+        self.connection_btn = self.findChild(QPushButton, 'connection_btn')
+        self.connection_btn.clicked.connect(self.telegram_connect)
+
+    def telegram_connect(self):
+        phone_number = self.phone_txt.text()
+        if phone_number is None or phone_number == '':
+            self.show_message_box("Warning", "Insert PhoneNumber")
+            return
+        client.connect()
+        if not client.is_user_authorized():
+            try:
+                client.send_code_request(phone_number)
+            except TypeError:
+                self.show_message_box("Warning", "Invalid Number")
+            code, ok = QInputDialog.getText(self, "Authorize", "", QLineEdit.Normal)
+            if ok:
+                try:
+                    me = client.sign_in(phone_number, code)
+                except SessionPasswordNeededError:
+                    self.show_message_box("Warning", "Need Password")
+                    return
+                except PhoneNumberUnoccupiedError:
+                    self.show_message_box("Warning", "Sign Up")
+                    return
+        window.show_telegram()
+
+    def show_message_box(self, title, message):
+        msg = QMessageBox()
+        msg.setWindowTitle(title)
+        msg.setText(message)
+        retval = msg.exec_()
+
+
+class TelegramWidget(QWidget):
+    def __init__(self, parent=None):
+        super(TelegramWidget, self).__init__(parent)
+        uic.loadUi(GUI_DIR + '/' + telegram_ui_file, self)
         self.chat_list = []
         self.cat_number = None
         self.chat_id = {}
+        self.category_id = {}
         self.entity_id = None
         self.chat_selected = False
 
-        # GUI
-        self.extract_btn = self.findChild(QtWidgets.QPushButton, 'extract_btn')
-        self.start_time_widget = self.findChild(QtWidgets.QDateTimeEdit, 'start_time_widget')
+        self.extract_btn = self.findChild(QPushButton, 'extract_btn')
+        self.start_time_widget = self.findChild(QDateTimeEdit, 'start_time_widget')
         self.start_time_widget.setDateTime(datetime.now())
-        self.end_time_widget = self.findChild(QtWidgets.QDateTimeEdit, 'end_time_widget')
+        self.end_time_widget = self.findChild(QDateTimeEdit, 'end_time_widget')
         self.end_time_widget.setDateTime(datetime.now())
-        self.chat_tbl_widget = self.findChild(QtWidgets.QTableWidget, 'chat_tbl')
+        self.chat_tbl_widget = self.findChild(QTableWidget, 'chat_tbl')
         self.chat_tbl_widget.cellClicked.connect(self.table_chat_click)
         self.extract_btn.clicked.connect(self.extractButtonClick)
-
         with client:
-            client.loop.run_until_complete(self.get_chat_list(phone))
+            client.loop.run_until_complete(self.get_chat_list())
         self.add_chat_table(self.chat_list)
-
-        self.show()
+        # self.show()
 
     def table_chat_click(self, row, column):
-        if column == 1:
+        if column == 0:
             self.chat_selected = True
-            category = self.chat_tbl_widget.item(row, 0).text()
-            chat_name = self.chat_tbl_widget.item(row, 1).text()
-            if category == 'user':
-                self.cat_number = 0
-            elif category == 'channel':
-                self.cat_number = 1
-            elif category == 'group':
-                self.cat_number = 2
-            else:
-                pass
+            chat_name = self.chat_tbl_widget.item(row, 0).text()
+            self.cat_number = self.category_id[chat_name]
             self.entity_id = self.chat_id[chat_name]
         else:
             self.chat_selected = False
@@ -98,8 +149,7 @@ class Ui(QtWidgets.QMainWindow):
             numrows = self.chat_tbl_widget.rowCount()
             self.chat_tbl_widget.setRowCount(numrows)
             # self.chat_tbl_widget.setColumnCount(4)
-            self.chat_tbl_widget.setItem(numrows - 1, 0, QtWidgets.QTableWidgetItem(chat['category']))
-            self.chat_tbl_widget.setItem(numrows - 1, 1, QtWidgets.QTableWidgetItem(chat['chat_name']))
+            self.chat_tbl_widget.setItem(numrows - 1, 0, QTableWidgetItem(chat['chat_name']))
 
     def extractButtonClick(self):
         start_time = self.start_time_widget.dateTime()
@@ -112,26 +162,14 @@ class Ui(QtWidgets.QMainWindow):
                 client.loop.run_until_complete(self.get_messages(self.entity_id, self.cat_number, start_time, end_time))
 
     def show_message_box(self, title, message):
-        msg = QtWidgets.QMessageBox()
+        msg = QMessageBox()
         msg.setWindowTitle(title)
         msg.setText(message)
         retval = msg.exec_()
 
-    async def get_chat_list(self, phone):
-        await client.start()
+    async def get_chat_list(self):
+        # await client.start()
         print("Client Created")
-        # Ensure you're authorized
-        if await client.is_user_authorized() == False:
-            self.show_message_box("Error", "Auth Failed...")
-            return
-            # await client.send_code_request(phone)
-            # try:
-            #     await client.sign_in(phone, input('Enter the code: '))
-            # except SessionPasswordNeededError:
-            #     await client.sign_in(password=input('Password: '))
-
-        me = await client.get_me()
-
         for chat in await client.get_dialogs():
             print('name:{0} id:{1} is_user:{2} is_channel:{3} is_group:{4}'.format(chat.name, chat.id, chat.is_user, chat.is_channel, chat.is_group))
             if chat.is_user:
@@ -145,6 +183,7 @@ class Ui(QtWidgets.QMainWindow):
                 chat_id = str(chat.id)[1:]
                 cat_number = 2
             self.chat_id[chat.name] = chat.id
+            self.category_id[chat.name] = cat_number
             each_chat = dict(category=categories[cat_number], chat_name=chat.name, chat_id=chat_id)
             print(each_chat)
             self.chat_list.append(each_chat)
@@ -195,20 +234,8 @@ class Ui(QtWidgets.QMainWindow):
                             )
                             try:
                                 text = tess.image_to_string(Image.open(download_path))
-                            except Exception as e :
+                            except Exception as e:
                                 self.show_message_box("Error", "No Tesseract")
-                            # if 'photo' in message.media:
-                            #     photo = message.media.photo
-                            #     photo_1 = Image.open(photo)
-                            #     image_buf = BytesIO()
-                            #     photo_1.save(image_buf, format="JPEG")
-                            #     image = image_buf.getvalue()
-                            # elif 'document' in message.media.key():
-                            #     photo = message.media.document
-                                # photo_1 = Image.open(photo)
-                                # image_buf = BytesIO()
-                                # photo_1.save(image_buf, format="JPEG")
-                                # image = image_buf.getvalue()
                         else:
                             continue
                         sn = sn + 1
@@ -226,17 +253,13 @@ class Ui(QtWidgets.QMainWindow):
                             direction = 'Origin'
                         row = [sn, direction, text, message.date.strftime("%Y-%m-%d %H:%M:%S")]
                         writer.writerow(row)
-
-                            # all_messages.append(message.to_dict())
                 offset_id = messages[len(messages) - 1].id
-                # total_messages = len(all_messages)
-                # if total_count_limit != 0 and total_messages >= total_count_limit:
-                #     break
         self.show_message_box("Success", code)
         return
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
+    app = QApplication(sys.argv)
+    window = MainUI()
+    window.show()
     app.exec_()
--- utils/constants.py
+++ utils/constants.py
@@ -4,11 +4,16 @@ import json
 # PATHS
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.join(_cur_dir, os.pardir)
-LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 CRED_DIR = os.path.join(ROOT_DIR, 'credentials')
 GUI_DIR = os.path.join(ROOT_DIR, 'gui')
 CSV_DIR = os.path.join(ROOT_DIR, 'csv')
+if not os.path.exists(CSV_DIR):
+    os.mkdir(CSV_DIR)
 MEDIA_DIR = os.path.join(ROOT_DIR, 'media')
+if not os.path.exists(MEDIA_DIR):
+    os.mkdir(MEDIA_DIR)
 credential_file = "config.ini"
-gui_file = "telegram_client.ui"
+main_ui_file = "main.ui"
+telegram_ui_file = "telegram.ui"
+login_ui_file = "login.ui"
 
